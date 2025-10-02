7f8753e06e305966d3fc1d0eb42da8c8716558be
I-Have-No-Idea-What-IAmDoing <55067948+I-Have-No-Idea-What-IAmDoing@users.noreply.github.com>
2025-10-02 00:53:29 -0500

    Update env.py

--- manga_ocr_dev/env.py
+++ manga_ocr_dev/env.py
@@ -4,11 +4,12 @@ This file defines the environment variables and paths used in the development sc
 
 from pathlib import Path
 
+
 ROOT_DIR = Path(__file__).parent.parent
 
 ASSETS_PATH = ROOT_DIR / "assets"
 FONTS_ROOT = ROOT_DIR / "fonts"
 DATA_SYNTHETIC_ROOT = ROOT_DIR / "assets"
-BACKGROUND_DIR = ROOT_DIR / "tmp" / "backgrounds"
+BACKGROUND_DIR = ROOT_DIR / "manga_ocr_dev" / "data" / "background"
 MANGA109_ROOT = ROOT_DIR / "assets"
-TRAIN_ROOT = ROOT_DIR / "out"
+TRAIN_ROOT = ROOT_DIR / "manga_ocr_dev" / "out"
